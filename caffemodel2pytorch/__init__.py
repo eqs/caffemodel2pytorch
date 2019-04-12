7e86deb581b56caae3212d63f23bb753475864cb
eqs <i11murasige@gmail.com>
2019-04-12 15:18:58 +0900

    add files need to python module

---
+++ caffemodel2pytorch/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 12 14:55:03 2019
+
+@author: eqs
+"""
+
+from .caffemodel2pytorch import *
+
---
+++ caffemodel2pytorch/__main__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 12 15:15:52 2019
+
+@author: Satoshi Murashige
+"""
+
+from .caffemodel2pytorch import main
+
+if __name__ == '__main__':
+    main()
+
